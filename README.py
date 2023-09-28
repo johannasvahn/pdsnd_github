e63b370466c62db1f549c4873712ad74fd77aa59
johanna svahn <johanna.svahn@volvo.com>
2023-09-28 15:27:59 +0200

    exclude csv files

---
+++ README.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 17 08:03:19 2023
+
+@author: yi45092
+"""
+
+"I have used the following sources for my project"
+
+
+
+#General information about Python, different scrips and error codes:
+    Explore-US-Bikeshare-Data - Udacity Data Analyst Degree
+    https://www.w3schools.com/python/default.asp
+    https://github.com/
+    https://www.geeksforgeeks.org/python-programming-language/?ref=shm_outind
+    
+
+While loops:
+https://www.w3schools.com/python/python_while_loops.asp
+https://www.geeksforgeeks.org/how-to-use-while-true-in-python/
+
+Error handling:
+https://www.w3schools.com/python/python_try_except.asp
+    
